131479fd5fdb57dd47ab884364ae7a82f21afc25
Ramiro Tepehua <rtcface@gmail.com>
2025-08-13 17:00:38 -0600

    se agregan endpoints

---
+++ functions/config/api-urls.js
@@ -0,0 +1,37 @@
+const urls = [
+    {
+        url: 'https://www.tony.com.mx/arte-y-manualidades',
+        name: 'Tony - Arte y Manualidades'        
+    },
+    {
+        url: 'https://www.tony.com.mx/computo-e-impresion',
+        name: 'Tony - Cómputo e Impresión'
+    },{
+        url:'https://www.tony.com.mx/electr%C3%B3nica',
+        name: 'Tony - Electrónica'
+    },
+    {
+        url:'https://www.tony.com.mx/escolar',
+        name: 'Tony - Escolar'
+    },
+    {
+        url:'https://www.tony.com.mx/higiene-y-limpieza',
+        name: 'Tony - Higiene y Limpieza'
+    },
+    {
+        url:'https://www.tony.com.mx/muebles-y-pizarrones',
+        name: 'Tony - Muebles y Pizarrones'
+    },{
+        url:'https://www.tony.com.mx/oficina',
+        name: 'Tony - Oficina'
+    },
+    {
+        url:'https://www.tony.com.mx/papel',
+        name: 'Tony - Papel'
+    },
+    {
+        url:'https://www.tony.com.mx/regalos-y-fiestas',
+        name: 'Tony - Regalos y Fiestas'
+    }
+
+]
